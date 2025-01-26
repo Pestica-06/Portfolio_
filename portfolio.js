12cd46950e6ebf29265503e2c0aaba76561e8a42
Pestica Mariyasiluvaimichal <pmariyasiluvaimichal@freshworks.com>
2025-01-26 18:56:59 +0530

    change

--- portfolio.js
+++ portfolio.js
@@ -1,6 +1,5 @@
-const hamburger = document.getElementById("hamburger");
-const navLinks = document.getElementById("nav-links");
-
-hamburger.addEventListener("click", () => {
-  navLinks.classList.toggle("active");
+document.getElementById('hamburger').addEventListener('click', () => {
+  const navLinks = document.getElementById('nav-links');
+  navLinks.classList.toggle('active');
+  document.getElementById('hamburger').classList.toggle('active');
 });
